24702896d2f1fbff86e58491f087e4c4429b449a
David Emily <daeth5@mail.missouri.edu>
2018-08-04 18:53:01 -0500

    Add files via upload
    added way to search by position. will add in greedy knapsack algorithm to choose best team.

--- csvScanner.py
+++ csvScanner.py
@@ -2,11 +2,14 @@ import csv
 from collections import namedtuple
 ##Reads the CSV File##
 
-def sortResult(result, attribute, ):
-    result.sort(key=lambda x: getattr(x,attribute))
+def sortResult(result):
+    print()
+    print("Position    |    Name    |    Price    |    Average Score    |    Price per Point")
+    result.sort(key=lambda x: getattr(x,"efficiency"))
 
 def prompt():
-    choice = input("Enter 1 for SP\n"+
+    choice = input("Will display data ranked on price per point from lowest to highest\n"+
+            "Enter 1 for SP\n"+
             "Enter 2 for 1B\n"+
             "Enter 3 for 2B\n"+
             "Enter 4 for SS\n"+
@@ -14,20 +17,25 @@ def prompt():
             "Enter 6 for C\n"+
             "Enter 7 for OF\n"+
             "Enter 8 for every position\n"+
-            "Enter 9 for every position but NOT sorted\n"+
+            "Enter 9 to send data to knapsack\n"+
             "Enter 0 to quit:"+
             "\n")
     return choice
+
+def greedyKnapsack(result):
+    print("\nData sent to knapsack to begin computing...")
+
 
 def safe_div(x,y):
     if float(y) == 0:
         return 0
     return float(x)/float(y)
 
-Player = namedtuple('Player', 'position name avg_score efficiency')
+Player = namedtuple('Player', 'position name price avg_score efficiency')
 result = []
 
 print("Begin reading in csv file..")
+
 with open('DKSalaries.csv','r') as csvFile:
     csvReader = csv.reader(csvFile, dialect='excel')
     next(csvReader)
@@ -35,34 +43,87 @@ with open('DKSalaries.csv','r') as csvFile:
         result.append(Player(
             position = row[0],
             name = row[2],
+            price = row[5],
             avg_score = row[8],
             efficiency = safe_div(row[5],row[8])
             ))
-
 csvFile.close()    
 
 print("Data has been loaded..")
+
 print("Displaying data:")
 
 choice = prompt()
 
 while choice != "0":
 
+    if choice == "1":
+        sortResult(result)
+        for person in result:
+            if "SP" in person.position and int(person.efficiency) > 0:
+                for stat in person:
+                    print(stat, end ="          ")
+                print()
+
+    if choice == "2":
+        sortResult(result)
+        for person in result:
+            if "1B" in person.position and int(person.efficiency) > 0:
+                for stat in person:
+                    print(stat, end ="          ")
+                print()
+    
+    if choice == "3":
+        sortResult(result)
+        for person in result:
+            if "2B" in person.position and int(person.efficiency) > 0:
+                for stat in person:
+                    print(stat, end ="          ")
+                print()
+    
+    if choice == "4":
+        sortResult(result)
+        for person in result:
+            if "SS" in person.position and int(person.efficiency) > 0:
+                for stat in person:
+                    print(stat, end ="          ")
+                print()
+
+    if choice == "5":
+        sortResult(result)
+        for person in result:
+            if "3B" in person.position and int(person.efficiency) > 0:
+                for stat in person:
+                    print(stat, end ="          ")
+                print()
+    
+    if choice == "6":
+        sortResult(result)
+        for person in result:
+            if "C" in person.position and int(person.efficiency) > 0:
+                for stat in person:
+                    print(stat, end ="          ")
+                print()
+
+    if choice == "7":
+        sortResult(result)
+        for person in result:
+            if "OF" in person.position and int(person.efficiency) > 0:
+                for stat in person:
+                    print(stat, end ="      ")
+                print()
+
     if choice == "8":
-        print()
-        sortResult(result, "efficiency")
-        print("Position    |    Name    |    Average Score    |    Price per Point")
+        sortResult(result)
         for person in result:
             if int(person.efficiency) > 0:
                 for stat in person:
-                    print(stat, end =" ")
+                    print(stat, end ="          ")
                 print()
 
     if choice == "9":
-        for person in result:
-            for stat in person:
-                print(stat, end =" ")
-            print()
+        greedyKnapsack(result)
+
     
     print()
     choice = prompt()
